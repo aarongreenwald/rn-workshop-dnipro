b0e890eae386852bb2557aa86860cb15bc2a4fe3
Aaron Greenwald <aarong@wix.com>
2016-09-07 20:22:33 +0300

    reuse code, our first component

--- MemeBuilder/index.ios.js
+++ MemeBuilder/index.ios.js
@@ -3,51 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import {AppRegistry} from 'react-native';
+import HomeScreen from './src/HomeScreen';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class MemeBuilder extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          I've started!!
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('MemeBuilder', () => MemeBuilder);
+AppRegistry.registerComponent('MemeBuilder', () => HomeScreen);
